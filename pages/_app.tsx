5dd01050c4db8066f9e7f6016916fb3278529ad6
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-10-08 15:05:32 -0500

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import type { ConfigOptions } from '@web3modal/react'
+import { Web3Modal } from '@web3modal/react'
+
+
+
+const config: ConfigOptions = {
+  projectId: '9338b83ff89083cd0ee7da8cea475c23',
+  theme: 'dark',
+  accentColor: 'default',
+  ethereum: {
+    appName: 'web3Modal'
+  }
+}
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+  <Component {...pageProps} />
+  <Web3Modal config={config} />
+  </>)
 }
 
 export default MyApp
